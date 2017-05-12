0e95033b2867a2d359d44f1d8f2682d161147c80
Roberto Osorio-Goenaga <rogoenaga@paperlesspost.com>
2017-05-12 10:08:37 -0400

    listing 3.9

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 'use strict';
 var http = require('http');
 var express = require('express');
+var path = require('path');
 var logger = require('morgan');
 var app = express();
 
 app.use(logger("short"));
 
+var publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
 app.use(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello world!");
-})
+    res.end("Looks like you didn't find a static file");
+});
 
 http.createServer(app).listen(3000);
