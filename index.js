7d79862bf985b56a838333f493fa0c80fba25978
Roberto Osorio-Goenaga <rogoenaga@paperlesspost.com>
2017-05-12 10:29:10 -0400

    listing 3.10

--- index.js
+++ index.js
@@ -10,9 +10,21 @@ app.use(logger("short"));
 var publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
+app.get("/", function(req, res) {
+    res.end("Welcome to my homepage!");
+});
+
+app.get("/about", function(req, res) {
+    res.end("Welcome to the about page!");
+});
+
+app.get("/weather", function(req, res) {
+    res.end("The current weather is NICE!");
+});
+
 app.use(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Looks like you didn't find a static file");
+    res.statusCode = 404;
+    res.end("FOE OH FOE");
 });
 
 http.createServer(app).listen(3000);
