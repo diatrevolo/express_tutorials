2aa941ab65caad92e4b6c72246ca47caba547923
Roberto Osorio-Goenaga <rogoenaga@paperlesspost.com>
2017-05-12 10:00:51 -0400

    listing 3.8

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 'use strict';
 var http = require('http');
 var express = require('express');
+var logger = require('morgan');
 var app = express();
 
-app.use(function(req, res, next) {
-    var minute = (new Date() ).getMinutes();
-    if ( (minute % 2) === 0 ) {
-        next();
-    } else {
-        res.statusCode = 403;
-        res.end("Not authorized.");
-    }
-});
+app.use(logger("short"));
 
 app.use(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end('Secret info: the password is "swordfish!');
-});
+    res.end("Hello world!");
+})
 
 http.createServer(app).listen(3000);
